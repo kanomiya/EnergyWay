f1d93877bf5048fbcd86bbf8f1908e9e098cb8a9
kanomiya <kanomiya@users.noreply.github.com>
2016-04-01 01:12:13 +0900

    EPE 取得メソッド

--- src/main/java/com/kanomiya/mcmod/energyway/api/EnergyWayAPI.java
+++ src/main/java/com/kanomiya/mcmod/energyway/api/EnergyWayAPI.java
@@ -2,8 +2,12 @@ package com.kanomiya.mcmod.energyway.api;
 
 import java.util.Map;
 
+import net.minecraft.entity.Entity;
+import net.minecraftforge.common.IExtendedEntityProperties;
+
 import com.google.common.collect.Maps;
 import com.kanomiya.mcmod.energyway.api.energy.EnergyType;
+import com.kanomiya.mcmod.energyway.api.props.EntityPropertiesEnergy;
 
 /**
  * @author Kanomiya
@@ -24,6 +28,15 @@ public class EnergyWayAPI {
 	{
 		if (! idToEnergyType.containsKey(id)) return null;
 		return idToEnergyType.get(id);
+	}
+
+
+
+	public static EntityPropertiesEnergy getProperties(Entity entity)
+	{
+		IExtendedEntityProperties props = entity.getExtendedProperties(EnergyWayAPI.ID_DATA);
+		if (props instanceof EntityPropertiesEnergy) return (EntityPropertiesEnergy) props;
+		return null;
 	}
 
 }
