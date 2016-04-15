db835150badb0421ea2ae143a2615d981a161e85
kanomiya <kanomiya@users.noreply.github.com>
2016-04-16 00:18:47 +0900

    Edit mcmod.info

--- src/main/java/com/kanomiya/mcmod/energyway/api/energy/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author Kanomiya
- *
- */
-@API(apiVersion = "alpha", owner = "EnergyWayAPI|core", provides = "EnergyWayAPI|energy")
-package com.kanomiya.mcmod.energyway.api.energy;
-import net.minecraftforge.fml.common.API;
-
--- src/main/java/com/kanomiya/mcmod/energyway/api/event/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author Kanomiya
- *
- */
-@API(apiVersion = "alpha", owner = "EnergyWayAPI|core", provides = "EnergyWayAPI|event")
-package com.kanomiya.mcmod.energyway.api.event;
-import net.minecraftforge.fml.common.API;
-
